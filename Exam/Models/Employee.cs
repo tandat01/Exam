bc2df29bdf85d45611c01f77eb78b6a32508eec1
tandat01 <dat01101999@gmail.com>
2020-03-17 20:37:09 +0700

    code

---
+++ Exam/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace Exam.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+
+        public int Salary { get; set; }
+    }
+}
